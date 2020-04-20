d77e0690409412d7661531b0b12431fdecc1817e
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2020-04-20 12:12:25 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# logger
+
+logger from cucumber framework
---
+++ logger.js
@@ -0,0 +1,56 @@
+/**
+ * Abstraction for global logger
+ */
+const { createLogger, format, transports } = require('winston');
+
+const {
+  colorize, combine, timestamp, printf,
+} = format;
+
+const log = createLogger({
+  level: 'debug',
+  format: combine(format.splat(), format.simple()),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new transports.File({
+      filename: `${process.cwd()}/reports/logs/error.log`,
+      level: 'error',
+      format: format.combine(
+        format.timestamp({
+          format: 'YYYY-MM-DD', // Optional for choosing your own timestamp format.
+        }),
+        format.json(),
+      ),
+    }),
+    new transports.File({
+      filename: `${process.cwd()}/reports/logs/combined.log`,
+      level: 'debug',
+      format: format.combine(
+        format.timestamp({
+          format: 'YYYY-MM-DD', // Optional for choosing your own timestamp format.
+        }),
+        format.json(),
+      ),
+    }),
+  ],
+});
+
+// Define your custom format with printf.
+const myFormat = printf(
+  (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+);
+//
+// If we're not in production then log to the `console`
+//
+if (process.env.NODE_ENV !== 'production') {
+  log.add(
+    new transports.Console({
+      format: combine(timestamp(), colorize(), myFormat),
+    }),
+  );
+}
+
+module.exports = { log };
