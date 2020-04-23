9f073ed13bd8704d94a0b911a0a52501e1e2bf12
Thomas Dsilva <thomas.dsilva.contractor@macmillan.com>
2020-04-23 14:52:43 +0530

    added date time to config

--- config.js
+++ config.js
@@ -14,7 +14,9 @@ const browser = (argv.browser || rc.selenium.browser);
 const headless = (argv.h || argv.headless === 'true' || rc.selenium.headless);
 const timeout = rc.selenium.timeout * 1000;
 const hub = getHub();
+const grid = (argv.grid || rc.selenium.grid || 'local');
+const datetime = new Date().toISOString();
 
 module.exports = {
-  browser, headless, timeout, hub,
+  browser, headless, timeout, hub, grid, datetime
 };
